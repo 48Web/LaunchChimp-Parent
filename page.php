19ce79c093fa2b53d216541577e8dd6d16a918dc
abrudtkuhl <abrudtkuhl@gmail.com>
2010-09-20 21:16:41 -0500

    added sidebar to page.php

--- page.php
+++ page.php
@@ -3,17 +3,20 @@
 <?php get_header(); ?>
 
 <?php if (have_posts()): while (have_posts()): the_post(); ?>
-
-	<h1><?php the_title(); ?></h1>
-	
-	<div class="meat">
-		<?php echo the_content(); ?>
-	</div>
-	
-	<?php if(comments_open()): ?>
-		<div id="comments">
-			<?php comments_template(); ?>
+	<div id="content">
+		<div class="post">
+			<h1><?php the_title(); ?></h1>
+			<div class="post-content">
+				<?php echo the_content(); ?>
+			</div>
+			<?php if(comments_open()): ?>
+				<div id="comments">
+					<?php comments_template(); ?>
+				</div>
+			<?php endif; ?>
 		</div>
-	<?php endif; ?>
-
+	</div><!--#end content-->
+	<div id="sidebar">
+		<?php get_sidebar(); ?>
+	</div><!--#end sidbear-->
 <?php endwhile; endif; get_footer(); ?>
