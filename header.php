3648423ef114413b356b760175ae85b9f831f659
abrudtkuhl <abrudtkuhl@gmail.com>
2010-10-01 16:13:27 -0500

    refactored markup

--- header.php
+++ header.php
@@ -60,4 +60,3 @@
 				</ul>
 			</div>
 		</div><!--#end header-->
-		
--- page.php
+++ page.php
@@ -2,21 +2,24 @@
 
 <?php get_header(); ?>
 
-<?php if (have_posts()): while (have_posts()): the_post(); ?>
 	<div id="content">
-		<div class="post">
-			<h1><?php the_title(); ?></h1>
-			<div class="post-content">
-				<?php echo the_content(); ?>
-			</div>
-			<?php if(comments_open()): ?>
-				<div id="comments">
-					<?php comments_template(); ?>
-				</div>
-			<?php endif; ?>
-		</div>
-	</div><!--#end content-->
-	<div id="sidebar">
+	    <?php if (have_posts()): while (have_posts()): the_post(); ?>
+            <div class="post">
+                <h1><a href="<?php the_permalink() ?>" rel="permalink"><?php the_title(); ?></a></h1>
+                <div class="post-content">
+                    <?php echo the_content(); ?>
+                </div>
+
+                <?php if(comments_open()): ?>
+                    <div id="comments">
+                        <?php comments_template(); ?>
+                    </div>
+                <?php endif; ?>
+		    </div><!--#end post-->
+        <?php endwhile; endif; ?>
+	</div><!--#end content -->
+	<div class="sidbear">
 		<?php get_sidebar(); ?>
-	</div><!--#end sidbear-->
-<?php endwhile; endif; get_footer(); ?>
+	</div><!--#end sidebar -->
+
+<?php get_footer(); ?>
