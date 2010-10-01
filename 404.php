3fa8edb2221bb48fe4e49f43d57a5d7ad59e255b
abrudtkuhl <abrudtkuhl@gmail.com>
2010-10-01 16:42:26 -0500

    updated 404 page

--- 404.php
+++ 404.php
@@ -1,17 +1,9 @@
-<?php
-add_action('wp_head', 'page_head');
-add_action('wp_footer', 'page_foot');
-get_header();
-function page_head(){?><?php }
-function page_foot(){?><?php }?>
-
-	<div id="content">
-		<div class="post">
+<?php get_header(); ?>
+   <div id="content" >
+     <div class="post">
 			<h1>404 Uh-Oh!</h1>
 			<p>Sorry, we were unable to find the page you were looking for. </p>
 		</div>
-	</div><!--#end content-->
-	<div id="sidebar">
-		<?php get_sidebar(); ?>
-	</div><!--#end sidebar-->
+	</div>
+<?php get_sidebar(); ?>
 <?php get_footer(); ?>
